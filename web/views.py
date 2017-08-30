13b07301afbb60fb67fbaa707598ce09c2b92444
lmb <2397989881@qq.com>
2017-08-30 23:56:17 +0800

    增加栏目列表页 分页

--- web/views.py
+++ web/views.py
@@ -1,14 +1,46 @@
 #coding=utf-8
 
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render,HttpResponseRedirect,render_to_response,RequestContext
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import logout,login,authenticate
 from forms import ArticleForm,CommentForm,RegistForm
 #注册时导入User表
 from django.contrib.auth.models import User
-
+#引入分页
+from django.core.paginator import Paginator,PageNotAnInteger, EmptyPage
 
 import models
+
+
+def my_pagination(request, queryset, display_amount=5, after_range_num = 5,bevor_range_num = 4):
+    #按参数分页
+    paginator = Paginator(queryset, display_amount)
+    try:
+        #得到request中的page参数
+        page =int(request.GET.get('page'))
+    except:
+        #默认为1
+        page = 1
+    try:
+        #尝试获得分页列表
+        objects = paginator.page(page)
+    #如果页数不存在
+    except EmptyPage:        #获得最后一页
+        objects = paginator.page(paginator.num_pages)
+    #如果不是一个整数
+    except:
+        #获得第一页
+        objects = paginator.page(1)
+    #根据参数配置导航显示范围
+    if page >= after_range_num:
+        page_range = paginator.page_range[page-after_range_num:page+bevor_range_num]
+    else:
+        page_range = paginator.page_range[0:page+bevor_range_num]
+    return objects,page_range
+
+
+
+
 
 # Create your views here.
 
@@ -20,8 +52,9 @@ def index(req):
 def lanmu(req,id):
 
     articles = models.Article.objects.filter(categroy_id=id)
-
-    return render(req,'index.html',{'articles':articles})
+    objects, page_range = my_pagination(req, articles)
+    #return render(req,'index.html',{'articles':articles,'page_range':page_range})
+    return render_to_response('list.html',{'articles':objects,'page_range':page_range},context_instance=RequestContext(req))
 
 def article(req,id):
 
