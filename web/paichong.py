f8959bb92b871320efe29cdbb215a9a4e04871ab
lmb <540032146@qq.com>
2017-09-07 17:38:43 +0800

    爬虫测试

--- web/paichong.py
+++ web/paichong.py
@@ -1,11 +1,27 @@
 #coding=utf-8
-
+import cookielib
 import urllib2
+import bs4
 
-request = urllib2.Request("http://www.kejiwang.cn")
+url = "http://www.baidu.com"
 
-try:
-    response = urllib2.urlopen(request,timeout=3)
-except urllib2.URLError,e:
-    print e.reason
-print response.read()
+print "Frist Method"
+response1 = urllib2.urlopen(url)
+print response1.getcode()
+print len(response1.read())
+
+print "Second Method"
+request = urllib2.Request(url)
+request.add_header("user-agent","Mozilla/5.0")
+response2 = urllib2.urlopen(request)
+print response2.getcode()
+print len(response2.read())
+
+print "Third Method"
+ck = cookielib.CookieJar()
+openner = urllib2.build_opener(urllib2.HTTPCookieProcessor(ck))
+urllib2.install_opener(openner)
+response3 = urllib2.urlopen(url)
+print response3.getcode()
+print ck
+print response3.read()
---
+++ web/test_bs.py
@@ -0,0 +1,43 @@
+#coding=utf-8
+from bs4 import BeautifulSoup
+import re
+
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc,'html.parser',from_encoding='utf-8')
+
+print '获取所有链接'
+links = soup.find_all('a')
+print links
+for link in links:
+    print link.name,link['href'],link.get_text(),link['class'],link['id']
+
+print '获取其中一个链接'
+linkone = soup.find('a',href="http://example.com/lacie")
+linkid = soup.find('a',id="link1")
+print linkone
+print '按id获取链接'
+print linkid
+
+print '按正则表达式'
+linkzz = soup.find('a',href=re.compile(r"cie"))
+print linkzz
+
+print '按class获取'
+textp = soup.find('p',class_='title')
+print textp,textp
+print textp.name
+print textp.get_text()
