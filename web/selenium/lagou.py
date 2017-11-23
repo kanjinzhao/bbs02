0819553b3f88ab221e96203024ce8433142b7c83
lmb <540032146@qq.com>
2017-11-23 17:40:32 +0800

    selenium测试

---
+++ web/selenium/lagou.py
@@ -0,0 +1,65 @@
+#coding=utf-8
+# @Time    : 17-11-23 下午3:02
+# @Author  : LIUMINGBO
+# @Email   : 540032146@qq.com
+import urllib2
+
+import time
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import bs4 as bs
+from bs4 import BeautifulSoup
+
+
+
+def main():
+    # 设置 user agent
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap['phantomjs.page.settings.userAgent'] = (
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
+    )
+    driver = webdriver.PhantomJS(desired_capabilities=dcap)
+
+    # 通过主页面获取相关session
+    #driver.get('http://www.lagou.com')
+    #driver.get('https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=')
+    #driver.get('http://bbs.qyer.com')
+    driver.get('http://bbs.qyer.com/forum-2-1.html')
+
+    # 解析页面
+    soup = bs.BeautifulSoup(driver.page_source, 'html.parser')
+    # 使用 css 选择器
+    #items = soup.select('.con_list_item')
+    items = soup.select('.imgwp')
+
+    # 输出招聘信息
+    x=1
+    for item in items:
+        print('*' * 40)
+        #print(item['data-company'])
+        #print(item['data-positionname'])
+        #print(item['data-salary'])
+        #print(item)
+        imgcode = item.find('img')
+        # 下载图片
+        url = imgcode['src']
+        #tim = int(time.time())
+
+        try:
+            f = open('/home/lmb/bbs02/static/uploads/'+str(x)+'.jpg','w')
+            req = urllib2.urlopen(url)
+            buf = req.read()
+            f.write(buf)
+            x=x+1
+            print x
+            #print (imgcode)
+            print (item)
+        except:
+            print ('异常')
+
+
+if __name__ == '__main__':
+    main()
--- web/selenium/test.py
+++ web/selenium/test.py
@@ -3,18 +3,32 @@
 # @Author  : LIUMINGBO
 # @Email   : 540032146@qq.com
 
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 browser = webdriver.Firefox()
 
-browser.get('http://www.baidu.com')
+browser.get('http://bbs.qyer.com/forum-2-1.html')
 
-elem = browser.find_element_by_name("wd")
-word = '科技网'.decode('utf-8')
-elem.send_keys(word)
-elem.send_keys(Keys.RETURN)
+#elem = browser.find_element_by_name("wd")
+#word = '科技网'.decode('utf-8')
+#elem.send_keys(word)
+#elem.send_keys(Keys.RETURN)
+#time.sleep(5)
+#browser.find_element_by_class_name("n").click()
+
+num = 1
+while (num < 9):
+    print '第'+ str(num) +'页'
+    browser.find_element_by_xpath("//a[contains(text(),'下一页')]").click()
+    time.sleep(5)
+
+    num = num + 1
+
+
+
+#elem.send_keys("",Keys.ARROW_DOWN)
 
 print browser.page_source
